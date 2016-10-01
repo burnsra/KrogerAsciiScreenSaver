57b230c203f6256e45ef55497616226a4df4b324
Robert Burns <burnsra@gmail.com>
2016-10-01 07:55:03 -0400

    Initial Commit

---
+++ SwiftScreenSaverTemplate/SwiftScreenSaverTemplateView.h
@@ -0,0 +1,13 @@
+//
+//  SwiftScreenSaverTemplateView.h
+//  SwiftScreenSaverTemplate
+//
+//  Created by Robert Burns on 10/1/16.
+//  Copyright © 2016 Robert Burns. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SwiftScreenSaverTemplateView : ScreenSaverView
+
+@end
